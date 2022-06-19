cde36922dac060db4c3e4b4d0e5173fb8eb9b102
nostalgialee <842109551@qq.com>
2022-06-19 17:28:57 +0800

    多个装饰器

--- derectors.py
+++ derectors.py
@@ -6,6 +6,9 @@
 
 # 装饰器相关
 
+
+# 一、functools.wraps 相关
+
 def tracer(func):
 
     def wrapper(*args, **kwargs):
@@ -23,20 +26,13 @@ def fibonacci(n):
 
 
 fibonacci(3)
-
-
 # fibonacci((1,),{})->1
 # fibonacci((0,),{})->0
 # fibonacci((2,),{})->1
 # fibonacci((1,),{})->1
 # fibonacci((3,),{})->2
-
-
-
 print(fibonacci)
 # <function tracer.<locals>.wrapper at 0x000001E2FF090950>
-
-
 print('help:') # help:
 help(fibonacci)
 # Help on function wrapper in module __main__:
@@ -80,15 +76,97 @@ fibonacci(3)
 # fibonacci((2,),{})->1
 # fibonacci((1,),{})->1
 # fibonacci((3,),{})->2
-
-
 print(fibonacci)
 # <function fibonacci at 0x0000023B11F609D8>
-
-
-
 print('help:') # help:
 help(fibonacci)
-
 # Help on function fibonacci in module __main__:
 # fibonacci(n)
+
+
+
+# 二、多个装饰器执行顺序
+
+def war1(func):
+    print('war1')
+    def inner(*args, **kwargs):
+        print('=======war1 start=======')
+        func(*args, **kwargs)
+        print('=======war1 end=======')
+    return inner
+
+
+def war2(func):
+    print('war2')
+    def inner(*args, **kwargs):
+        print('=======war2 start=======')
+        func(*args, **kwargs)
+        print('=======war2 end=======')
+    return inner
+
+@war1
+@war2
+def func():
+    print('func runs')
+
+
+func()
+
+
+"""
+装饰器函数的执行顺序：
+    被装饰函数的定义阶段
+    被装饰函数的执行阶段
+    装饰器函数在被装饰函数定义好后立刻执行
+    
+        函数定义阶段：执行顺序从最靠近函数的装饰器开始，自内而外的执行
+        函数执行阶段：执行顺序由外而内，一层层执行
+    
+"""
+
+# 1. 当被装饰函数，被定义 而 未被执行的时候，装饰器函数开始执行
+# @war1
+# @war2
+# def func():
+#     print('func runs')
+
+# func = war1(func)
+# func = war2(func)
+
+
+
+"""
+
+被装饰函数定义阶段：
+    例子中：
+    先走 war2
+    后走 war1
+war1(war2(func))
+
+war2(f) 作为被装饰函数 放入 war1 中
+
+内部 inner 中的伪代码
+    def inner_b(*args, **kwargs):
+        print('=======war1 start=======')
+        war2(func)
+        print('=======war1 end=======')
+
+被装饰函数执行阶段：
+    例子中 
+    先走 b
+    后走 a
+
+打印结果：
+
+
+'war2'
+'war1'
+'=======war1 start======='
+'=======war2 start======='
+'func runs'
+'=======war2 end======='
+'=======war1 end======='
+
+
+"""
+
